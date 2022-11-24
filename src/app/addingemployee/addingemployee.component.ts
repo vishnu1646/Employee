fd8e59e54b2aede7fb28a337a7496c2dafc91280
vishnu1646 <vishnulance98@gmail.com>
2022-11-25 01:21:37 +0530

    adding

---
+++ src/app/addingemployee/addingemployee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addingemployee',
+  templateUrl: './addingemployee.component.html',
+  styleUrls: ['./addingemployee.component.css']
+})
+export class AddingemployeeComponent {
+
+}
