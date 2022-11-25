545a9880b8e149b197b6baa4e99320cc3b5f9cc7
vishnu1646 <vishnulance98@gmail.com>
2022-11-25 11:01:38 +0530

    databinding

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent {
+  data:any= [
+    {
+      "name":"Vishnu",
+  "designation":"Software Engineer",
+  "company":"NeST Digital",
+  "salary":"45000",
+    },
+    {
+        "name":"Athul",
+    "designation":"Software Engineer",
+    "company":"EY",
+    "salary":"35000",
+    },
+    {
+      "name" :"Ajay",
+  "designation":"Software Engineer",
+  "company":"TCS",
+  "salary":"40000",
+    },
+    {
+      "name" :"Nerrin",
+  "designation":"Software Engineer",
+  "company":"UST",
+  "salary":"45000",
+    },
+  {
+    "name" :"Karthika",
+"designation":"Software Engineer",
+"company":"Google",
+"salary":"75000",
+  }
+  ]
 
 }
